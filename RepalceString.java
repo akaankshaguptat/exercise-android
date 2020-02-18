e93256efd23a731066f59d5a3d073d23cfe283de
akaankshaguptat <60652132+akaankshaguptat@users.noreply.github.com>
2020-02-18 21:55:42 +0530

    Delete RepalceString.java

--- RepalceString.java
+++
@@ -1,9 +0,0 @@
-//Q1. Write a program to replace a substring inside a string with other string ?
-
-public class RepalceString {
-    public static void main(String[] args) {
-        String s1="hello";
-      String s2=s1.replace("el","eldsdfs");
-        System.out.println(s2);
-    }
-}
