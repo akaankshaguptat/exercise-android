b3d3d79369976b58db56df43122b054a9f58a129
akaanksha <akaanksha.gupta@tothenew.com>
2020-03-19 20:18:16 +0530

    commited web service exercise

---
+++ app/src/main/java/com/example/webservices/DisplayListView.java
@@ -0,0 +1,14 @@
+package com.example.webservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_list_view);
+    }
+}
