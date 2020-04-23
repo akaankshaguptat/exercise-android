154af6b4afcc43f9cbeb5f2182e416107dc84c3f
akaanksha <akaanksha.gupta@tothenew.com>
2020-04-23 15:19:35 +0530

    permission added

--- app/src/main/java/com/example/intents/Explicit.java
+++ app/src/main/java/com/example/intents/Explicit.java
@@ -1,8 +1,13 @@
 package com.example.intents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -12,9 +17,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Explicit extends AppCompatActivity {
-    TextView textView3,textView4,textView5,textView6;
-    EditText editText2;
-    Button button;
+    TextView user_text,email_text,phone_text,pass_text;
+    EditText url;
+    Button browse,permissionBtn;
+     final int CAMERA_CODE=100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +31,66 @@ public class Explicit extends AppCompatActivity {
         String email=intent.getStringExtra(MainActivity.EXTRA_EMAIL);
         int phone=intent.getIntExtra(MainActivity.EXTRA_PHONE,0);
         String pass=intent.getStringExtra(MainActivity.EXTRA_PASS);
-        textView3=findViewById(R.id.textView3);
-        textView4=findViewById(R.id.textView4);
-        textView5=findViewById(R.id.textView5);
-        textView6=findViewById(R.id.textView6);
-        editText2=findViewById(R.id.editText2);
-        button=(Button)findViewById(R.id.button);
+        user_text=findViewById(R.id.textView3);
+        email_text=findViewById(R.id.textView4);
+        phone_text=findViewById(R.id.textView5);
+        pass_text=findViewById(R.id.textView6);
+        url=findViewById(R.id.editText2);
+        browse=findViewById(R.id.button);
+        permissionBtn=findViewById(R.id.permissionbtn);
 
-        textView3.setText(user_name);
-        textView4.setText(email);
-        textView5.setText(""+phone);
-        textView6.setText(pass);
+        user_text.setText(user_name);
+        email_text.setText(email);
+        phone_text.setText(""+phone);
+        pass_text.setText(pass);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        browse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              String url=  editText2.getText().toString();
-              if(!url.equals(""))
+              String url1=  url.getText().toString();
+              if(!url1.equals(""))
               {
-                  Intent intent1=new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+                  Intent intent1=new Intent(Intent.ACTION_VIEW,Uri.parse(url1));
                   startActivity(intent1);
               }
               else
                   Toast.makeText(Explicit.this,"Please enter the url",Toast.LENGTH_LONG).show();
             }
         });
+        permissionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                permissions();
+            }
+        });
 
+    }
+
+    public void permissions() {
+
+
+
+        if (ContextCompat.checkSelfPermission(Explicit.this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(Explicit.this,
+                    new String[]{Manifest.permission.CAMERA}, CAMERA_CODE);
+        }
+        else {
+            Toast.makeText(this, "Permissions granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==CAMERA_CODE){
+            if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
